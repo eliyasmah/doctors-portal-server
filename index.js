9be40681f10f6387f8a396a79e5afe1fd63ce4bb
eliyasmah <eliyasbubt@gmail.com>
2022-05-21 02:51:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.clnsq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+    const bookingCollection = client.db("doctors_portal").collection("booking");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+      const services = await serviceCollection.find().toArray();
+
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      services.forEach((service) => {
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        service.slots = available;
+      });
+      res.send(services);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        data: booking.data,
+        patient: booking.patient,
+      };
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello From doctor");
+});
+
+app.listen(port, () => {
+  console.log(`Doctors app listening on port ${port}`);
+});
